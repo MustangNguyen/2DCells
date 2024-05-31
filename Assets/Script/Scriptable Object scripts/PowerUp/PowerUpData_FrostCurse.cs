53634754f5c6ac0bf6d879d724a409cddf97b23b
Sotatek-QuyNguyen3 <157369326+Sotatek-QuyNguyen3@users.noreply.github.com>
2024-05-31 16:47:35 +0700

    feat: add power up elechain

--- Assets/Script/Scriptable Object scripts/PowerUp/PowerUpData_FrostCurse.cs
+++ Assets/Script/Scriptable Object scripts/PowerUp/PowerUpData_FrostCurse.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Power Up Data Frost Curse", menuName = "Scriptable Objects/Power Up Data/Frost Curse")]
 public class PowerUpData_FrostCurse : PowerUpData
 {
-    public FrostCurseUpgrades[] FrostCurseUpgrades = new FrostCurseUpgrades[6];
+    public FrostCurseUpgrades[] frostCurseUpgrades = new FrostCurseUpgrades[6];
 
     public override string UpdateDescription(int lv){
         base.UpdateDescription(lv);
-        description = $"Form a <color=#00ff00>{FrostCurseUpgrades[lv].radius}</color> m ice field around mutation, cause <color=#00ff00>{FrostCurseUpgrades[lv].damage}%</color> DPS per <color=#00ff00>{FrostCurseUpgrades[lv].delay}s</color>";
+        description = $"Form a <color=#00ff00>{frostCurseUpgrades[lv].radius}</color> m ice field around mutation, cause <color=#00ff00>{frostCurseUpgrades[lv].damage}%</color> DPS per <color=#00ff00>{frostCurseUpgrades[lv].delay}s</color>";
         return description;
     }
 }
