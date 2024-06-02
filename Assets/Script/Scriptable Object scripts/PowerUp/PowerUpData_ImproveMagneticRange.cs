603abe5e76081374060aee1a828dc79648c03e5d
MustangNguyen <124245922+MustangNguyen@users.noreply.github.com>
2024-06-02 21:23:08 +0700

    feat: magnetic power up card

---
+++ Assets/Script/Scriptable Object scripts/PowerUp/PowerUpData_ImproveMagneticRange.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Power Up Data Improve Magnetic Range", menuName = "Scriptable Objects/Power Up Data/Improve Magnetic Range")]
+public class PowerUpData_ImproveMagneticRange : PowerUpData
+{
+    public MagneticUpgrades[] magneticUpgrades = new MagneticUpgrades[6];
+    public override string UpdateDescription(int lv){
+        base.UpdateDescription(lv);
+        description = $"Increase obs collect radius to <color=#00ff00>{magneticUpgrades[lv].radius}%</color>";
+        return description;
+    }
+}
+[SerializeField]
+public class MagneticUpgrades{
+    public float radius;
+}
